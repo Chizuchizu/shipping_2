7c821f796d4b20b8b413abb76938fa75c176b5d4
chizuchizu <ochimasumi@gmail.com>
2020-11-08 21:07:58 +0900

    mlflow対応

--- src/prophet_v1.py
+++ src/prophet_v1.py
@@ -5,6 +5,8 @@ from sklearn.metrics import mean_squared_error as mse
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import mlflow
+import pickle
 
 from src.load_base_data import preprocessed_data
 
@@ -32,9 +34,11 @@ train = load_data()
 data = pd.DataFrame()
 best_score = 0
 use_all_data = True
+rand = np.random.randint(0, 1000000)
+experiment_name = f"{rand}"
 
 
-def run_model(pars):
+def run_model():
     all_score = 0
     for x in ["SC1", "SC2", "SC3"]:
         if not use_all_data:
@@ -49,41 +53,62 @@ def run_model(pars):
             use_data = train.loc[train["shipping_company"] == x, ["ds", "y"]]
             periods = 61
 
-        model = build_model(pars)
-        model.fit(use_data)
-        future = model.make_future_dataframe(periods=periods)
-        forecast = model.predict(future)
+        # mlflow.set_experiment(experiment_name)
+        mlflow.set_experiment(x)
 
-        oof = forecast["yhat"].iloc[:-periods]
-        val_y = use_data["y"]
+        # with mlflow.start_run(run_name=f"{x}"):
+        with mlflow.start_run(run_name=f"{rand}"):
+            model = build_model()
+            model.fit(use_data)
+            future = model.make_future_dataframe(periods=periods)
+            forecast = model.predict(future)
 
-        score = np.exp(-np.sqrt(mse(oof, val_y)))
-        print("score: ", score)
-        all_score += score
+            oof = forecast["yhat"].iloc[:-periods]
+            val_y = use_data["y"]
 
-        pred = forecast[["ds", "yhat"]]
-        # if x == "SC2":
-        pred = pred.iloc[-61:, :]
-        # else:
-        #     pred = pred.iloc[]
-        pred["company"] = x
+            score = np.exp(-np.sqrt(mse(oof, val_y)))
+            print("score: ", score)
+            all_score += score
 
-        plot_model(model, forecast, pars, x)
+            pred = forecast[["ds", "yhat"]]
+            # if x == "SC2":
+            pred = pred.iloc[-61:, :]
+            # else:
+            #     pred = pred.iloc[]
+            pred["company"] = x
 
-        if x == "SC1":
-            data = pred
-        else:
-            data = pd.concat([data, pred])
+            # https://github.com/facebook/prophet/issues/725
+            pkl_path = f"../models/{rand}_{x}_prophet.pkl"
+            with open(pkl_path, "wb") as f:
+                pickle.dump(model, f)
+
+            # plot_model(model, forecast, pars, x)
+            fig1 = model.plot(forecast)
+            plt.savefig("fig1.png")
+            plt.clf()
+            fig2 = model.plot_components(forecast)
+            plt.savefig("fig2.png")
+            plt.clf()
+
+            if x == "SC1":
+                data = pred
+            else:
+                data = pd.concat([data, pred])
+
+            mlflow.log_param("score", score)
+            mlflow.log_artifact("fig1.png")
+            mlflow.log_artifact("fig2.png")
+
     all_score /= 3
 
     data = data.sort_values(["ds", "company"])
     return all_score, data["yhat"]
 
 
-def build_model(pars):
-    year_list = [2019, 2020]
-    holidays = make_holidays_df(year_list=year_list, country='UK')
-    wseas, mseas, yseas, s_prior, h_prior, c_prior = pars
+def build_model():
+    # year_list = [2019, 2020]
+    # holidays = make_holidays_df(year_list=year_list, country='UK')
+    # wseas, mseas, yseas, s_prior, h_prior, c_prior = pars
     m = Prophet(growth='linear',
                 # holidays=holidays,
                 # daily_seasonality="auto",
@@ -97,12 +122,12 @@ def build_model(pars):
     m = m.add_seasonality(
         name='weekly',
         period=7,
-        fourier_order=wseas)
+        fourier_order=15)
 
     m = m.add_seasonality(
         name='monthly',
         period=30.5,
-        fourier_order=mseas)
+        fourier_order=25)
 
     # m = m.add_seasonality(
     #     name='yearly',
@@ -112,17 +137,25 @@ def build_model(pars):
     return m
 
 
-params = [[15, 25, 50, 0.1, 0.1, 0.1]]
-best_params = None
-for pars in params:
-    # m = build_model(pars)
-    score, forecast = run_model(pars)
+#
+# params = [[15, 25, 50, 0.1, 0.1, 0.1]]
+# best_params = None
+# for pars in params:
+#     # m = build_model(pars)
+#     score, forecast = run_model(pars)
+#
+#     if best_score < score:
+#         best_score = score
+#         best_params = pars
+#         best_forecast = forecast
 
-    if best_score < score:
-        best_score = score
-        best_params = pars
-        best_forecast = forecast
+score, forecast = run_model()
 
-print(best_score, best_params)
+print(score)
 forecast[forecast < 0] = 0
-# forecast.to_csv(f"../outputs/{round(best_score, 5)}_prophet_non_year.csv", index=False, header=False)
+forecast.to_csv(f"../outputs/{round(best_score, 5)}_prophet_non_year.csv", index=False, header=False)
+
+mlflow.set_experiment("all")
+with mlflow.start_run(run_name=f"{rand}"):
+    mlflow.log_param("score", score)
+    mlflow.log_artifact(f"../outputs/{round(best_score, 5)}_prophet_non_year.csv")
